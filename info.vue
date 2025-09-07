6d32349504cb88924d6dc42adcc1f9773986bd66
A-Awad1 <eng.a.awad1@gmail.com>
2025-09-07 14:08:31 +0300

    small update

---
+++ info.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Written in HTML with semantic and accessible markup. -->
+</template>
+
+<script setup lang="ts">
+/*
+Uses the Composition API with TypeScript for strong typing and cleaner logic.
+*/
+</script>
+
+<style lang="scss">
+/* 
+Written in SCSS (not scoped), leveraging global SCSS helpers, variables, mixins, and placeholders to maintain design consistency. 
+*/
+</style>
+
+<!-- 
+All Vue components in the project follow a consistent structure, clean code and coding standards to ensure readability, scalability, and maintainability. The Code Length in Each component is limited to a maximum of 200 lines (enforced by ESLint). 
+-->
